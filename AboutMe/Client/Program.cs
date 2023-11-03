7e2fe4ed48a4f3c04c749e3e5bcacd9c986296ae
Luca Bösinger <extern.luca.boesinger@duerrdental.com>
2023-11-03 15:06:18 +0100

    Updated

--- AboutMe/Client/Program.cs
+++ AboutMe/Client/Program.cs
@@ -1,9 +1,7 @@
 using AboutMe.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+using Radzen;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,13 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
-
-builder.Services
-   .AddBlazorise(options =>
-   {
-      options.Immediate = true;
-   })
-   .AddBootstrapProviders()
-   .AddFontAwesomeIcons();
